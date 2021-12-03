a7451932cc8b62cfe5247301178ed67938fab4e7
Rick James <rkj@fb.com>
2021-12-02 21:50:14 -0800

    day 3 part 2

--- 3/b.py
+++ 3/b.py
@@ -8,30 +8,81 @@ import math
 data = open("data.txt", "r")
 lines = data.readlines()
 
-depth = 0
-forward = 0
-aim = 0
+length = 0
+infile = []
 
 for line in lines:
     if line == "\n":
         continue
 
-    s = line.split(" ")
+    if length == 0:
+        length = len(line.strip())
 
-    cmd = s[0]
-    delta = int(s[1])
+    num = int(line, 2)
+    infile.append(num)
 
-    if cmd == "forward":
-        forward += delta
-        depth += aim * delta
+# returns tuple <count ones>, <count zeros>
+def count(l, mask):
 
-    if cmd == "down":
-        aim += delta
+    # count
+    ones = 0
+    zeros = 0
 
-    if cmd == "up":
-        aim -= delta 
+    for e in l:
+        if e & mask != 0:
+            ones += 1
+        else:
+            zeros += 1
+
+    return ones, zeros
+
+# filters l at mask on target
+def filter(l, mask, target):
+    out = []
+    for e in l:
+        if (e & mask == 0 and target == 0) or (e & mask != 0 and target == 1):
+            out.append(e)
+
+    return out
+
+oxy = infile
+for i in reversed(range(length)):
+    mask = 1 << i
+    ones, zeros = count(oxy, mask)
+
+    target = 0
+    if ones > zeros:
+        target = 1
+    if ones == zeros:
+        target = 1
+
+    oxy = filter(oxy, mask, target)
+
+    if len(oxy) == 1:
+        break
 
 
-print("depth", depth)
-print("forward", forward)
-print("product", forward*depth)
+co = infile
+
+for i in reversed(range(length)):
+    mask = 1 << i
+    ones, zeros = count(co, mask)
+
+    target = 0
+    if ones < zeros:
+        target = 1
+    if ones == zeros:
+        target = 0 
+
+    co = filter(co, mask, target)
+
+    if len(co) == 1:
+        break
+
+print("oxy len", len(oxy))
+print("oxy 0", bin(oxy[0]))
+
+print("co len", len(co))
+print("co 0", bin(co[0]))
+
+print("product", oxy[0] * co[0])
