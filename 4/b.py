f411345abe246ba532dbb396aa9b6890aa77edb5
Rick James <rkj@fb.com>
2021-12-04 07:28:29 -0800

    broken part 2

--- 4/b.py
+++ 4/b.py
@@ -8,81 +8,137 @@ import math
 data = open("data.txt", "r")
 lines = data.readlines()
 
-length = 0
-infile = []
+draw = []
+s = lines[0].split(",")
+for e in s:
+    draw.append(int(e))
 
-for line in lines:
+print("lendraw", len(draw), "draw", draw)
+
+boards = []
+board = []
+
+for i in range(1, len(lines)):
+    line = lines[i]
     if line == "\n":
         continue
 
-    if length == 0:
-        length = len(line.strip())
+    s = line.split()
+    row = []
+    for e in s:
+        row.append(int(e))
 
-    num = int(line, 2)
-    infile.append(num)
+    if not len(row) == 5:
+        print("bad row length")
 
-# returns tuple <count ones>, <count zeros>
-def count(l, mask):
+    board.append(row)
 
-    # count
-    ones = 0
-    zeros = 0
+    if len(board) == 5:
+        boards.append(board)
+        board = []
 
-    for e in l:
-        if e & mask != 0:
-            ones += 1
-        else:
-            zeros += 1
+#print("boards", boards)
+print("lenboards", len(boards))
 
-    return ones, zeros
+# hits n on all boards
+hits = set()
+def hit(n):
+    for i in range(len(boards)):
+        for x in range(5):
+            for y in range(5):
+                if n == boards[i][x][y]:
+                    if n == 64 and i == 63:
+                        print("hitting 64 on board 63", boards[i][x][y])
+                    hits.add((i,x,y))
 
-# filters l at mask on target
-def filter(l, mask, target):
-    out = []
-    for e in l:
-        if (e & mask == 0 and target == 0) or (e & mask != 0 and target == 1):
-            out.append(e)
+completed_set = set()
 
-    return out
+def finished():
 
-oxy = infile
-for i in reversed(range(length)):
-    mask = 1 << i
-    ones, zeros = count(oxy, mask)
+    for i in range(len(boards)):
+        debug = False
+        if i == 63 and len(hits) > 1100:
+            print("interesting start")
+            print("first record",  (63, 0, 0) in hits)
+            debug = True
 
-    target = 0
-    if ones > zeros:
-        target = 1
-    if ones == zeros:
-        target = 1
+        if i in completed_set:
+            continue
 
-    oxy = filter(oxy, mask, target)
+        found = False
 
-    if len(oxy) == 1:
+        # search rows
+        for x in range(5):
+            c = 0
+            for y in range(5):
+                if (i,x,y) in hits:
+                    c += 1
+                    if c == 5:
+                        found = True
+                else:
+                    if debug:
+                        print("miss ", (i,x,y))
+        if debug:
+            print("row found=", found)
+
+        # search columns
+        for y in range(5):
+            c = 0
+            for x in range(5):
+                if (i,x,y) in hits:
+                    c += 1
+                    if c == 5:
+                        found = True
+
+        if debug:
+            print("column found=", found)
+            if boards[i][0] == [64, 67, 11, 10, 92]:
+                print("interesting board found=", found, "i", i, "lenhits", len(hits))
+
+        # remember board and report sum of unmarked
+        if found:
+            completed_set.add(i)
+            s = 0
+            for x in range(5):
+                for y in range(5):
+                    if not (i,x,y) in hits:
+                        s += boards[i][x][y]
+            return s
+
+    return 0
+
+complete = 0
+drawi = 0
+while True:
+
+    if drawi >= len(draw):
+        print("debug dump")
+        for i in range(len(boards)):
+            for x in range(5):
+                for y in range(5):
+                    if not (i,x,y) in hits:
+                        print("missing", (i,x,y))
+
+        print("completed_set", completed_set)
+
+        for i in range(len(boards)):
+            if not i in completed_set:
+                print("incomplete", boards[i])
+                break
+
+    hit(draw[drawi])
+    #print("drawi", drawi, "draw", draw[drawi], "lenhits", len(hits))
+    s = finished()
+    if not s == 0:
+        complete += 1
+        #print("completed", s, "drawi", drawi, "draw", draw[drawi], "product", s * draw[drawi], "complete", complete)
+        if not complete == len(completed_set):
+            print("odd", complete, len(completed_set))
+
+    if complete == len(boards):
+        print("lastsum", s, "draw", draw[drawi], "product", s * draw[drawi])
         break
 
+    drawi += 1
 
-co = infile
 
-for i in reversed(range(length)):
-    mask = 1 << i
-    ones, zeros = count(co, mask)
-
-    target = 0
-    if ones < zeros:
-        target = 1
-    if ones == zeros:
-        target = 0 
-
-    co = filter(co, mask, target)
-
-    if len(co) == 1:
-        break
-
-print("oxy len", len(oxy))
-print("oxy 0", bin(oxy[0]))
-
-print("co len", len(co))
-print("co 0", bin(co[0]))
-
-print("product", oxy[0] * co[0])
