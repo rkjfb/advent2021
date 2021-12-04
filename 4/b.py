39716ff22c115b49b2ff1c5046bdf1678cd64414
Rick James <rkj@fb.com>
2021-12-04 08:29:08 -0800

    day 4 part 2

--- 4/b.py
+++ 4/b.py
@@ -5,40 +5,47 @@ import copy
 from collections import deque
 import math
 
-data = open("data.txt", "r")
-lines = data.readlines()
-
 draw = []
-s = lines[0].split(",")
-for e in s:
-    draw.append(int(e))
-
-print("lendraw", len(draw), "draw", draw)
-
 boards = []
-board = []
 
-for i in range(1, len(lines)):
-    line = lines[i]
-    if line == "\n":
-        continue
+def parse():
 
-    s = line.split()
-    row = []
+    data = open("data.txt", "r")
+    lines = data.readlines()
+
+    # draw list
+    s = lines[0].split(",")
     for e in s:
-        row.append(int(e))
+        draw.append(int(e))
 
-    if not len(row) == 5:
-        print("bad row length")
+    print("lendraw", len(draw), "draw", draw)
 
-    board.append(row)
+    # board list
+    board = []
+    for i in range(1, len(lines)):
+        line = lines[i]
+        if line == "\n":
+            continue
 
-    if len(board) == 5:
-        boards.append(board)
-        board = []
+        s = line.split()
+        row = []
+        for e in s:
+            row.append(int(e))
 
-#print("boards", boards)
-print("lenboards", len(boards))
+        if not len(row) == 5:
+            print("bad row length")
+
+        board.append(row)
+
+        if len(board) == 5:
+            boards.append(board)
+            board = []
+
+    #print("boards", boards)
+    print("lenboards", len(boards))
+
+
+dump_everything = False
 
 # hits n on all boards
 hits = set()
@@ -47,22 +54,26 @@ def hit(n):
         for x in range(5):
             for y in range(5):
                 if n == boards[i][x][y]:
-                    if n == 64 and i == 63:
-                        print("hitting 64 on board 63", boards[i][x][y])
+                    if n == 92 and i == 63:
+                        print("hitting 92 on board 63", boards[i][x][y])
+                        global dump_everything
+                        dump_everything = True
                     hits.add((i,x,y))
 
-completed_set = set()
+complete = set()
 
 def finished():
 
+    if dump_everything:
+        print("running finished, len boards", len(boards))
+
     for i in range(len(boards)):
         debug = False
-        if i == 63 and len(hits) > 1100:
-            print("interesting start")
-            print("first record",  (63, 0, 0) in hits)
+        if i == 63 and dump_everything:
+            print("on board 63: (63,0,0) in hits",  (63, 0, 0) in hits)
             debug = True
 
-        if i in completed_set:
+        if i in complete:
             continue
 
         found = False
@@ -76,7 +87,7 @@ def finished():
                     if c == 5:
                         found = True
                 else:
-                    if debug:
+                    if debug and False:
                         print("miss ", (i,x,y))
         if debug:
             print("row found=", found)
@@ -97,48 +108,55 @@ def finished():
 
         # remember board and report sum of unmarked
         if found:
-            completed_set.add(i)
+            complete.add(i)
             s = 0
             for x in range(5):
                 for y in range(5):
                     if not (i,x,y) in hits:
                         s += boards[i][x][y]
+            if dump_everything:
+                print("early out for board", i)
             return s
 
     return 0
 
-complete = 0
-drawi = 0
-while True:
+def main():
+    parse()
 
-    if drawi >= len(draw):
-        print("debug dump")
-        for i in range(len(boards)):
-            for x in range(5):
-                for y in range(5):
-                    if not (i,x,y) in hits:
-                        print("missing", (i,x,y))
+    drawi = 0
+    while True:
 
-        print("completed_set", completed_set)
+        if drawi >= len(draw):
+            print("debug dump")
+            for i in range(len(boards)):
+                for x in range(5):
+                    for y in range(5):
+                        if not (i,x,y) in hits:
+                            print("missing", (i,x,y))
 
-        for i in range(len(boards)):
-            if not i in completed_set:
-                print("incomplete", boards[i])
-                break
+            print("complete", complete)
 
-    hit(draw[drawi])
-    #print("drawi", drawi, "draw", draw[drawi], "lenhits", len(hits))
-    s = finished()
-    if not s == 0:
-        complete += 1
-        #print("completed", s, "drawi", drawi, "draw", draw[drawi], "product", s * draw[drawi], "complete", complete)
-        if not complete == len(completed_set):
-            print("odd", complete, len(completed_set))
+            for i in range(len(boards)):
+                if not i in complete:
+                    print("incomplete", boards[i])
+                    break
 
-    if complete == len(boards):
-        print("lastsum", s, "draw", draw[drawi], "product", s * draw[drawi])
-        break
+        if dump_everything:
+            print("drawi", drawi)
+        hit(draw[drawi])
 
-    drawi += 1
+        s = 1
+
+        while s != 0:
+            # 1 draw might trigger >1 board
+            s = finished()
+
+            if len(complete) == len(boards):
+                print("completed", s, "draw", draw[drawi], "product", s * draw[drawi])
+                return
+
+        drawi += 1
+
+main()
 
 
