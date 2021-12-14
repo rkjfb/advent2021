35a96e412fd0c8b477f3d05184466daed077ffda
Rick James <rkj@fb.com>
2021-12-14 07:43:50 -0800

    day 14 part 2

--- 14/a.py
+++ 14/a.py
@@ -9,12 +9,6 @@ import math
 template = ""
 rules = {}
 
-# cache of CC->10 step expansion template->string
-cache_exp = {}
-
-#cache of CC->10 step expansion->count dict
-cache_dict= {}
-
 def parse():
     global template
     global rules
@@ -30,39 +24,6 @@ def parse():
         s = line.split(" -> ")
         rules[s[0]] = s[1]
 
-
-# returns expansion of t
-def step(t):
-    newt = ""
-    last = ""
-
-    for i in range(len(t)-1):
-        m = t[i:i+2]
-
-        n = m[0] + rules[m] 
-        last = m[1]
-
-        newt += n
-
-    return newt + last
-
-# populates cache for cc
-def gen(cc):
-    print("gen", cc)
-
-    t = cc
-    for i in range(1):
-        t = step(t)
-
-    cache_exp[cc] = t
-
-    count = collections.defaultdict(int)
-    for c in t:
-        count[c] += 1
-
-    global cache_dict
-    cache_dict[cc] = count
-
 def print_dict(d):
     dmax = max(d.values())
     cmax = max(d, key=d.get)
@@ -71,49 +32,61 @@ def print_dict(d):
 
     print("range", cmax, dmax, cmin, dmin, dmax - dmin)
 
-# returns the count dict of t expanded to depth
-def recurse(t, depth):
-    #print("start", depth, t)
-    last = ""
 
-    ret_dict = collections.defaultdict(int)
+cache = []
 
-    for i in range(len(t)-1):
-        m = t[i:i+2]
-        last = m[1]
+# returns dict of characters at depth for 2 character string
+def get(depth, cc):
+    if cc in cache[depth]:
+        return cache[depth][cc]
 
-        if not m in cache_exp:
-            gen(m)
+    if depth == 0:
+        d = collections.defaultdict(int)
+        for c in cc:
+            d[c] += 1
+    else:
+        mid = rules[cc]
+        lhs = get(depth-1, cc[0]+mid)
+        rhs = get(depth-1, mid+cc[1])
 
-        n = cache_exp[m] 
+        # create merged dict
+        d = lhs.copy()
+        for k,v in rhs.items():
+            d[k] += v
+        # double counted mid
+        d[mid] -= 1
 
-        if depth == 1:
-            d = cache_dict[m]
-            d[last] -= 1
-            #print("depth1", n)
-            #print_dict(d)
-            #print(d)
-        else:
-            d = recurse(n, depth - 1)
+    cache[depth][cc] = d
 
-        for k,v in d.items():
-            ret_dict[k] += v
+    return d
 
-    #ret_dict[last] += 1
-
-    #print("end", depth, t)
-    #print_dict(ret_dict)
-    #print(ret_dict)
-    return ret_dict
 
 def main():
     parse()
 
-    d = recurse(template, 10)
-    d[template[-1]] += 1
-    print("final")
-    print_dict(d)
+    for i in range(41):
+        cache.append(dict())
 
+    last = ""
+    final_dict = collections.defaultdict(int)
+    for i in range(len(template)-1):
+        m = template[i:i+2]
+        last = m[1]
+        d = get(40, m)
+
+        # merged 2 character dict in
+        for k,v in d.items():
+            final_dict[k] += v
+
+        # don't double count the last character
+        final_dict[last] -= 1
+
+    # the last iteration last character should be restored
+    final_dict[last] += 1
+
+    print("final")
+    print(final_dict)
+    print_dict(final_dict)
 
 main()
 
