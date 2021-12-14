5098472809dd60ea1c6b47904751ef3361e4955c
Rick James <rkj@fb.com>
2021-12-13 22:55:45 -0800

    day 14 part 2 broken

--- 14/a.py
+++ 14/a.py
@@ -9,6 +9,12 @@ import math
 template = ""
 rules = {}
 
+# cache of CC->10 step expansion template->string
+cache_exp = {}
+
+#cache of CC->10 step expansion->count dict
+cache_dict= {}
+
 def parse():
     global template
     global rules
@@ -25,37 +31,89 @@ def parse():
         rules[s[0]] = s[1]
 
 
-def step():
-    global template
-    global rules
-
+# returns expansion of t
+def step(t):
     newt = ""
     last = ""
 
-    for i in range(len(template)-1):
-        m = template[i:i+2]
+    for i in range(len(t)-1):
+        m = t[i:i+2]
 
         n = m[0] + rules[m] 
         last = m[1]
 
         newt += n
 
-    template = newt + last
+    return newt + last
 
-def trange():
+# populates cache for cc
+def gen(cc):
+    print("gen", cc)
+
+    t = cc
+    for i in range(1):
+        t = step(t)
+
+    cache_exp[cc] = t
+
     count = collections.defaultdict(int)
-    for c in template:
+    for c in t:
         count[c] += 1
 
-    print("range", max(count.values()) - min(count.values()))
+    global cache_dict
+    cache_dict[cc] = count
+
+def print_dict(d):
+    dmax = max(d.values())
+    cmax = max(d, key=d.get)
+    dmin = min(d.values())
+    cmin = min(d, key=d.get)
+
+    print("range", cmax, dmax, cmin, dmin, dmax - dmin)
+
+# returns the count dict of t expanded to depth
+def recurse(t, depth):
+    #print("start", depth, t)
+    last = ""
+
+    ret_dict = collections.defaultdict(int)
+
+    for i in range(len(t)-1):
+        m = t[i:i+2]
+        last = m[1]
+
+        if not m in cache_exp:
+            gen(m)
+
+        n = cache_exp[m] 
+
+        if depth == 1:
+            d = cache_dict[m]
+            d[last] -= 1
+            #print("depth1", n)
+            #print_dict(d)
+            #print(d)
+        else:
+            d = recurse(n, depth - 1)
+
+        for k,v in d.items():
+            ret_dict[k] += v
+
+    #ret_dict[last] += 1
+
+    #print("end", depth, t)
+    #print_dict(ret_dict)
+    #print(ret_dict)
+    return ret_dict
 
 def main():
     parse()
 
-    for i in range(10):
-        step()
+    d = recurse(template, 10)
+    d[template[-1]] += 1
+    print("final")
+    print_dict(d)
 
-    trange()
 
 main()
 
