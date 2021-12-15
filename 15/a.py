3c12229ad22507a99a43de2da01358ead3662572
Rick James <rkj@fb.com>
2021-12-14 22:21:47 -0800

    day 15 part 2 broken

--- 15/a.py
+++ 15/a.py
@@ -36,16 +36,41 @@ def parse():
     global topleft
     topleft = grid[(0,0)]
 
+# expands grid by 5x5
+def expand():
+    global grid
+    newgrid = collections.defaultdict(int)
+    width = maxx+1
+    height = maxy+1
+
+    for ox in range(5):
+        for oy in range(5):
+            for x in range(maxx+1):
+                for y in range(maxy+1):
+                    value = grid[(x,y)]
+                    delta = ox+oy
+                    newvalue = value+delta
+                    if newvalue > 9:
+                        newvalue -= 9
+                    newx = ox * width + x
+                    newy = oy * height + y
+                    newgrid[(newx,newy)] = newvalue
+
+    grid = newgrid
+
+
 # does a djikstra'ish mark from bot right to top left of cumulative path
 def mark():
-    for x in range(maxx,-1,-1):
-        for y in range(maxy,-1,-1):
+    megamaxx = (maxx+1)*5-1
+    megamaxy = (maxy+1)*5-1
+    for x in range(megamaxx,-1,-1):
+        for y in range(megamaxy,-1,-1):
             smaller = 0
-            if x == maxx and y == maxy:
+            if x == megamaxx and y == megamaxy:
                 smaller = 0
-            elif x == maxx:
+            elif x == megamaxx:
                 smaller = grid[(x,y+1)]
-            elif y == maxy:
+            elif y == megamaxy:
                 smaller = grid[(x+1,y)]
             else:
                 smaller = min(grid[(x,y+1)], grid[(x+1,y)])
@@ -54,6 +79,8 @@ def mark():
 
 def main():
     parse()
+    expand()
+    #print(grid)
     mark()
 
     print("mincost", grid[(0,0)]-topleft)
