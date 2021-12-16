7e73867343ad12c0e2c5a726e743505fc10f36f4
Rick James <rkj@fb.com>
2021-12-15 22:24:01 -0800

    day 16 part 2

--- 16/a.py
+++ 16/a.py
@@ -17,7 +17,7 @@ def pprint(p):
         s += "   "
 
     s += p
-    print(s)
+    #print(s)
 
 # converts hex char to string of exactly 4 binary
 def char2bin(c):
@@ -51,15 +51,15 @@ def literal(s):
 
     lit = int(lit,2)
     pprint("literal " + str(lit))
-    return s, 1
+    return s, lit
 
 # parses operator
 # returns remaining string, packets read
-def operator(s):
+def operator(t, s):
     length_id = s[0]
     s = s[1:]
 
-    packets_read = 0
+    data = []
 
     if length_id == "0":
         # length in bits
@@ -72,7 +72,7 @@ def operator(s):
 
         while start_len - len(s) < length_bits:
             s,read = packet(s)
-            packets_read += 1
+            data.append(read)
 
         pprint("finished, remainder " + s)
         
@@ -83,12 +83,49 @@ def operator(s):
         s = s[11:]
         pprint(s)
 
+        packets_read = 0
         while packets_read < length_packets:
             s,read = packet(s)
+            data.append(read)
             packets_read += 1
-            pprint("packets_read " + str(packets_read))
 
-    return s,packets_read
+    result = 0
+
+    if t == 0:
+        result = sum(data)
+    elif t == 1:
+        result = data[0]
+        for v in data[1:]:
+            result *= v
+    elif t == 2:
+        result = min(data)
+    elif t == 3:
+        result = max(data)
+    elif t == 5:
+        if len(data) != 2:
+            raise "wrong length for type 5 packet"
+        if data[0] > data[1]:
+            result = 1
+        else:
+            result = 0
+    elif t == 6:
+        if len(data) != 2:
+            raise "wrong length for type 6 packet"
+        if data[0] < data[1]:
+            result = 1
+        else:
+            result = 0
+    elif t == 7:
+        if len(data) != 2:
+            raise "wrong length for type 6 packet"
+        if data[0] == data[1]:
+            result = 1
+        else:
+            result = 0
+    else:
+        raise "unrecognized type " + t
+
+    return s,result
 
 # parses a random packet starting at s
 def packet(s):
@@ -98,16 +135,25 @@ def packet(s):
     s,ver,t = vertype(s)
     pprint("--- version " + str(ver) + " type " + str(t) + "---")
 
-    packets_read = 0
+    result = 0
 
     if t == 4:
-        s,packets_read = literal(s)
+        s,result = literal(s)
     else:
-        s,packets_read = operator(s)
+        s,result = operator(t, s)
 
     depth -= 1
 
-    return s,packets_read
+    return s,result
+
+def evaluate(line):
+    s = ""
+
+    for c in line:
+        s += char2bin(c)
+
+    s,result = packet(s)
+    return result
 
 def parse():
     global s
@@ -116,21 +162,36 @@ def parse():
     rlines = data.readlines()
 
     line = rlines[0].strip()
+    print("evaluate", evaluate(line))
 
-    s = ""
+def test():
+    if evaluate("C200B40A82") != 3:
+        print("failed C200B40A82")
 
-    for c in line:
-        s += char2bin(c)
+    if evaluate("04005AC33890") != 54:
+        print("failed 04005AC33890")
 
-    pprint(s)
+    if evaluate("880086C3E88112") != 7:
+        print("failed 880086C3E88112")
 
-    packet(s)
+    if evaluate("CE00C43D881120") != 9:
+        print("failed CE00C43D881120")
 
-    print("** version_sum", version_sum)
+    if evaluate("D8005AC2A8F0") != 1:
+        print("failed D8005AC2A8F0")
 
+    if evaluate("F600BC2D8F") != 0:
+        print("failed F600BC2D8F")
+
+    if evaluate("9C005AC2F8F0") != 0:
+        print("failed 9C005AC2F8F0")
+
+    if evaluate("9C0141080250320F1802104A08") != 1:
+        print("failed 9C0141080250320F1802104A08")
 
 def main():
     parse()
+    test()
 
 main()
 
