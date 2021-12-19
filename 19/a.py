d6c1593b2bee7cf65db1765a389a674a306abb9d
Rick James <rkj@fb.com>
2021-12-19 11:35:21 -0800

    day 19 part 1 scanner 0 matches scanner 1

--- 19/a.py
+++ 19/a.py
@@ -12,9 +12,20 @@ rotations = []
 
 class Scanner:
     def __init__(self, beacons):
+        # original beacons in scanner space, sorted by z,y,x
+        for i in range(2,-1,-1):
+            beacons = beacons[beacons[:,i].argsort()]
         self.beacons = beacons
+
+        # list of all rotations applied, sorted by z,y,x
         self.transformed = []
         self.build_transformed()
+
+        # Beacons, in root coordinate system
+        self.root_beacons = None
+        # Offset used to get beacons into root coordinate system
+        self.root_offset = np.array([-9999, -9999, -9999])
+
 
     # build all transformations of beacons, sorted by z,y,x
     def build_transformed(self):
@@ -34,6 +45,50 @@ class Scanner:
 
             self.transformed.append(result)
 
+    # returns True, and updates root_beacons and root_offset
+    def match(self, root_beacons):
+        for start_i in range(10):
+            for t in self.transformed:
+                for start_j in range(10):
+                    offset = root_beacons[start_i] - t[start_j]
+
+                    i = start_i 
+                    j = start_j 
+                    count = 0
+                    while i < len(root_beacons) and j < len(t) and count < 12:
+                        b = t[j] + offset
+                        if np.array_equal(root_beacons[i], b):
+                            count += 1
+                            if count > 1:
+                                print(count)
+                            i += 1
+                            j += 1
+                        # x
+                        elif root_beacons[i][0] < b[0]:
+                            i += 1
+                        elif b[0] < root_beacons[i][0]:
+                            j += 1
+                        # y
+                        elif root_beacons[i][1] < b[1]:
+                            i += 1
+                        elif b[1] < root_beacons[i][1]:
+                            j += 1
+                        # z
+                        elif root_beacons[i][2] < b[2]:
+                            i += 1
+                        elif b[2] < root_beacons[i][2]:
+                            j += 1
+                        else:
+                            print("root_beacons", root_beacons[i], "b0", b)
+                            assert False
+
+                    if count == 12:
+                        self.root_offset = offset
+                        self.root_beacons = t + offset
+                        return True
+
+        return False
+
 def parse():
     global scanner
 
@@ -51,7 +106,7 @@ def parse():
 
         if line[1] == "-":
             if len(beacons) > 0:
-                scanner.append(Scanner(beacons))
+                scanner.append(Scanner(np.array(beacons)))
                 beacons = []
         else:
             s = line.split(",")
@@ -91,6 +146,10 @@ def main():
     build_rotations()
     parse()
     print("len scanner", len(scanner))
-    print("scanner1.transformed", scanner[1].transformed)
+
+    print("match", scanner[1].match(scanner[0].beacons))
+    print("scanner1 root_beacons", scanner[1].root_beacons)
+
+
 
 main()
