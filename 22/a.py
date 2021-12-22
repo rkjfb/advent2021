bdcdf70537def0df1da6eaf7f6d1deabbc0dbedc
Rick James <rkj@fb.com>
2021-12-22 09:53:41 -0800

    day 22 part 2 little example pass

--- 22/a.py
+++ 22/a.py
@@ -7,18 +7,20 @@ import math
 #from scipy.spatial.transform import Rotation
 
 class Node:
-    def __init__(self, x,y,z, size):
+    def __init__(self, x,y,z, size, depth):
         # center of cube
         self.x = x
         self.y = y
         self.z = z
         # cube bounds (center-size/2)x(center+size/2)
-        assert math.log2(size).is_integer()
+        assert math.log2(size).is_integer(), "need to use power of 2 size"
+        assert size >= 1, "should have at least size 1"
         self.size = size
         # cube is entirely full
         self.full = False
         # 8 children or None
         self.children = None
+        self.depth = depth
 
     def max(self):
         halfsize = self.size //2
@@ -45,11 +47,11 @@ class Node:
         (ox1,ox2,oy1,oy2,oz1,oz2) = o
         (ix1,ix2,iy1,iy2,iz1,iz2) = i
 
-        if ix2 < ox1 or ix1 > ox2:
+        if ix2 <= ox1 or ix1 >= ox2:
             return False
-        if iy2 < oy1 or iy1 > oy2:
+        if iy2 <= oy1 or iy1 >= oy2:
             return False
-        if iz2 < oz1 or iz1 > oz2:
+        if iz2 <= oz1 or iz1 >= oz2:
             return False
 
         return True
@@ -60,25 +62,28 @@ class Node:
             return
 
         self.children = []
-        assert self.size >= 2
         halfsize = self.size//2
-        for x in [self.x-halfsize, self.x+halfsize]:
-            for y in [self.y-halfsize, self.y+halfsize]:
-                for z in [self.z-halfsize, self.z+halfsize]:
-                    self.children.append(Node(x,y,z,halfsize))
+        assert halfsize >= 1
+        quartersize = halfsize/2
+        assert quartersize >= 0.5
+        # print("instantiate children", halfsize, quartersize)
+        for x in [self.x-quartersize, self.x+quartersize]:
+            for y in [self.y-quartersize, self.y+quartersize]:
+                for z in [self.z-quartersize, self.z+quartersize]:
+                    self.children.append(Node(x,y,z,halfsize,self.depth+1))
 
     def add(self, p):
         if not self.intersects_(p, self.max()):
-            print(self.max(), "add", p, " - no intersection")
+            #print(self.max(), "add", p, " - no intersection")
             return
 
         if self.contains(p, self.max()):
-            print(self.max(), "add", p, " - full containment")
+            #print(self.max(), "add", p, " - full containment")
             self.full = True
             self.children = None
             return
 
-        print(self.max(), "add", p)
+        #print(self.max(), "add", p)
 
         # partial containment
         self.ensure_children_()
@@ -88,7 +93,7 @@ class Node:
 
     def subtract(self, p):
         if not self.intersects_(p, self.max()):
-            print(self.max(), "subtract", p, " - no intersection")
+            #print(self.max(), "subtract", p, " - no intersection")
             return
 
         if self.contains(p, self.max()):
@@ -99,8 +104,13 @@ class Node:
 
         print(self.max(), "subtract", p)
 
-        # partial containment
+        # partial intersection, allocate full children so we can intersect
         self.ensure_children_()
+
+        if self.full:
+            self.full = False
+            for c in self.children:
+                c.full = True
 
         for c in self.children:
             c.subtract(p)
@@ -119,15 +129,16 @@ class Node:
         return total
 
     def __repr__(self):
+        ret = str(self.depth) + ", " + str(self.size) + ": " 
         if self.full:
-            return "full"
-        return str(self.children)
+            return ret + "full"
+        return ret + str(self.children) 
 
-    def print_tree(self):
-        print(self)
+    def print_tree(self,depth):
+        print(depth, self)
         if self.children != None:
             for c in self.children:
-                c.print_tree()
+                c.print_tree(depth+1)
 
 def parse():
     global decoder
@@ -136,7 +147,7 @@ def parse():
     data = open("data.txt", "r")
     rlines = data.readlines()
 
-    root = Node(0,0,0,32)
+    root = Node(0,0,0,128, 0)
 
     for line in rlines:
         line = line.strip()
@@ -176,11 +187,12 @@ def parse():
         if on == 1:
             root.add(p)
         else:
-            print("subtract")
-            #root.subtract(p)
+            #print("skip subtract")
+            root.subtract(p)
 
     print("sum", root.sum())
-    #root.print_tree()
+    #print(root)
+    #root.print_tree(0)
 
 def main():
     parse()
